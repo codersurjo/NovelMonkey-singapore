601ee64db9d6b7ea1ea893d0d0987ec1f07c7386
codersurjo <codersurjo@gmail.com>
2023-04-05 09:20:38 +0600

    5-4-23

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    //Mobile menu
+    $('.mobile_btn').click(function(){
+        $(".header_menu").slideToggle();
+    })
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+
+    // Modal Video
+    var $videoSrc;
+    $('.btn-play').click(function () {
+        $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
+    $('#videoModal').on('shown.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+    $('#videoModal').on('hide.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc);
+    })
+
+
+});
